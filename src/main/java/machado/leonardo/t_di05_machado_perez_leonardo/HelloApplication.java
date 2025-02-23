9f3b1f8ba75665fd5d26e24eb3d3c9415aa1aa03
Leo-Carlos3 <2258466@alu.murciaeduca.es>
2025-02-23 18:03:11 +0100

    Se conecta a la base de datos :)

--- src/main/java/machado/leonardo/t_di05_machado_perez_leonardo/HelloApplication.java
+++ src/main/java/machado/leonardo/t_di05_machado_perez_leonardo/HelloApplication.java
@@ -6,6 +6,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class HelloApplication extends Application {
     @Override
@@ -18,6 +20,19 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        connect();
         launch();
     }
+
+    public static void connect() {
+        // connection string
+        var url = "jdbc:sqlite:chinook.db";
+
+        try (var conn = DriverManager.getConnection(url)) {
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module machado.leonardo.t_di05_machado_perez_leonardo {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens machado.leonardo.t_di05_machado_perez_leonardo to javafx.fxml;
     exports machado.leonardo.t_di05_machado_perez_leonardo;
