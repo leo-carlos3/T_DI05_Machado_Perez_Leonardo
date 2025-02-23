6e31c660ad8d87fd8bdfd34a09ed0615fac15b91
Leo-Carlos3 <2258466@alu.murciaeduca.es>
2025-02-23 18:02:26 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module machado.leonardo.t_di05_machado_perez_leonardo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens machado.leonardo.t_di05_machado_perez_leonardo to javafx.fxml;
+    exports machado.leonardo.t_di05_machado_perez_leonardo;
+}
